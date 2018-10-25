62d0871f137db39fc346ce6600a1ac3ba13eb85e
Katya Astakhova <KatyaAstakhova@coherentsolutions.com>
2018-10-25 16:37:48 +0300

    Added code for translation app

---
+++ Translation/Translation/EnToRu.cs
@@ -0,0 +1,26 @@
+﻿namespace Translation
+{
+    class EnToRu
+    {
+      public static string Translate(string en)
+       {
+
+            string upper = en.ToUpper();
+            string translated = string.Empty;
+
+            for (int i = 0; i < upper.Length; i++)
+            {
+                string value = string.Empty;
+                if (AddToDictionary.GenerateEnDict().TryGetValue(upper[i].ToString(), out value))
+                {
+                    translated += value;
+                }
+                else
+                {
+                    translated += upper[i];
+                }
+            }
+            return translated;
+        }
+    }
+}
